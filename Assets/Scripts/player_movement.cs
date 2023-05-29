305ced0bd0a9fe9cce36f5ebc45d2f9661eb64e0
mohammed fatayer <mhmad.jamal24@yahoo.com>
2023-05-29 08:05:49 +0300

    gravity update

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -7,6 +7,15 @@ public class player_movement : MonoBehaviour
 
     public CharacterController  controller;
     public float speed =5f;
+
+    Vector3 velocity;
+    public float gravity = -9.81f;
+
+    public bool isGrounded;
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundmask;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +25,11 @@ public class player_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+         isGrounded = Physics.CheckSphere(groundCheck.position ,groundDistance,groundmask);
+         if(isGrounded && velocity.y < 0)
+         {
+            velocity.y = -2f;
+         }
         float x = Input.GetAxis("Horizontal");
 
         float z = Input.GetAxis("Vertical");
@@ -24,6 +38,9 @@ public class player_movement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+        velocity.y +=gravity * Time.deltaTime;
+
+        controller.Move(velocity *Time.deltaTime);
 
 
 
