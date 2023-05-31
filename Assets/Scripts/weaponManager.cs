12d175f04d338390fa3128678ef4e4837165108b
mohammed fatayer <mhmad.jamal24@yahoo.com>
2023-05-31 17:47:12 +0300

    adding fire animation

--- Assets/Scripts/weaponManager.cs
+++ Assets/Scripts/weaponManager.cs
@@ -8,27 +8,40 @@ public class weaponManager : MonoBehaviour
      public GameObject playerCam;
      public float range=100f;
      public float damage =25f;
+     public float firerare =1f;
+
+     public Animator PlayerAnimator;
+
+     private float readyToFire;
     // Start is called before the first frame update
     void Start()
-    {
-        
+    { 
+      PlayerAnimator.SetInteger("Movement",0);
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if(Input.GetButtonDown("Fire1"))
+    {       
+        if (Time.time >= readyToFire)
         {
+           PlayerAnimator.SetInteger("Fire",-1);
+           PlayerAnimator.SetInteger("Movement",0);
+        }
+        if(Input.GetButtonDown("Fire1") && Time.time >= readyToFire)
+        { 
            Shoot();
+           PlayerAnimator.SetInteger("Fire",2);
+            PlayerAnimator.SetInteger("Movement",-1);
         }
         
     }
 
     void Shoot()
-    {
+    {  
+     
         RaycastHit hit;
 
-      if (  Physics.Raycast(playerCam.transform.position, transform.forward,out hit,range))
+      if (  Physics.Raycast(playerCam.transform.position,- transform.forward,out hit,range))
       {
        
              Debug.Log(" hit  ");
