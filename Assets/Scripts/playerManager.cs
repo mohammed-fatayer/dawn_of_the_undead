c20e49e8e4360d661bdbc5ce0ed13289bf64e7cc
Osamah Sharaf <aldeen.osamah@std.izu.edu.tr>
2023-06-01 01:14:41 +0300

    Update playerManager.cs
     fix some error

--- Assets/Scripts/playerManager.cs
+++ Assets/Scripts/playerManager.cs
@@ -1,33 +1,33 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.UI;
-// using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
-// public class playerManager : MonoBehaviour
-// {
-//     public float health =100;
-//     public Text healthText;
-//     public GameManager gameManager;
-//     public void hit(float damage){
-//         health -= damage;
-//         healthText.text=health.ToString()+" HP";
-//         if (health <= 0)
-//         {
-//             gameManager.EndGame();
-//         }
-//     }
-//     void Start()
-//     {
+public class playerManager : MonoBehaviour
+{
+    public float health =100;
+    public Text healthText;
+    public GameManager gameManager;
+    public void hit(float damage){
+        health -= damage;
+        healthText.text=health.ToString()+" HP";
+        if (health <= 0)
+        {
+            gameManager.EndGame();
+        }
+    }
+    void Start()
+    {
         
-//     }
+    }
 
-//     // Update is called once per frame
-//     void Update()
-//     {
+    // Update is called once per frame
+    void Update()
+    {
         
-//     }
-// }
+    }
+}
 
 
